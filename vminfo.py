205f6c5e31e2a04b9ebd8e7cdd1dd94e88a8c200
alexeykr65 <alexeykr@gmail.com>
2016-06-12 21:51:46 +0300

    New release 2.0

--- vminfo.py
+++ vminfo.py
@@ -1,135 +1,58 @@
 #!/usr/bin/env python
-import paramiko
+#
+# Get and set information from Vmware
+#
+# alexeykr@gmail.com
+#
 import sys
-import getopt
+import argparse
+import paramiko
 import re
 from colorama import Fore, Style
 
-
 host = '192.168.180.46'
 user = 'root'
-secret = ''
+secret = 'noncemale'
 port = 22
+description = "vminfo: Get and set information of VM from Vmware via ssh"
+epilog = "Alexey Karpov "
 
 
-def main(argv):
-    # print 'Argument List:', str(sys.argv)
-    try:
-        opts, args = getopt.getopt(argv, "hlso:r:k:p:q:", ["ifile=", "ofile="])
-    except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-l':
-            get_full_listvm()
-            sys.exit()
-        elif opt in ("-s",):
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=host, username=user, password=secret, port=port)
-            stdin, stdout, stderr = client.exec_command('esxcli vm process list')
-            data = stdout.read()
-            result_data = ''
-            for sT in data.splitlines():
-                print sT
-            client.close()
-            sys.exit()
-
-        elif opt in ("-r"):
-            argSt = arg
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=host, username=user, password=secret, port=port)
-            stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/getallvms | grep -i -E '+"\""+argSt+"\"\n")
-            data = stdout.read()
-            result_data = ''
-            for sT in data.splitlines():
-                # print sT
-                for sF in sT.split():
-                    # print sF
-                    break
-                sCom = 'vim-cmd vmsvc/power.on '+sF+"\n"
-                print "======================================="
-                print sCom
-                stdin, stdout, stderr = client.exec_command(sCom)
-                result_data = stdout.read() + stderr.read()
-                print result_data
-            client.close()
-            sys.exit()
-        elif opt in ("-p"):
-            argSt = arg
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=host, username=user, password=secret, port=port)
-            for sF in argSt.split(','):
-                print sF
-                sCom = 'vim-cmd vmsvc/power.on '+sF+"\n"
-                print "======================================="
-                print sCom
-                stdin, stdout, stderr = client.exec_command(sCom)
-                result_data = stdout.read() + stderr.read()
-                print result_data
-            client.close()
-            sys.exit()
-        elif opt in ("-q"):
-            argSt = arg
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=host, username=user, password=secret, port=port)
-            for sF in argSt.split(','):
-                print sF
-                sCom = 'vim-cmd vmsvc/power.off '+sF+"\n"
-                print "======================================="
-                print sCom
-                stdin, stdout, stderr = client.exec_command(sCom)
-                result_data = stdout.read() + stderr.read()
-                print result_data
-            client.close()
-            sys.exit()
-
-        elif opt in ("-k"):
-            argSt = arg
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(hostname=host, username=user, password=secret, port=port)
-            stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/getallvms | grep -i -E '+"\""+argSt+"\"\n")
-            data = stdout.read()
-            result_data = ''
-            for sT in data.splitlines():
-                for sF in sT.split():
-                    break
-                sCom = 'vim-cmd vmsvc/power.off '+sF+"\n"
-                print "======================================="
-                print sCom
-                stdin, stdout, stderr = client.exec_command(sCom)
-                result_data = stdout.read() + stderr.read()
-                print result_data
-            client.close()
-
-            sys.exit()
-
-        elif opt in ("-o", "--ofile"):
-            get_listvm(arg)
-            sys.exit()
+def cmdArgsParser():
+    parser = argparse.ArgumentParser(description=description, epilog=epilog)
+    parser.add_argument('-k', '--killpid', help='Terminate vm with PIDs', action="store")
+    parser.add_argument('-l', '--listvm', help='List vm', action="store_true")
+    parser.add_argument('-p', '--procvm', help='Process list on vmware', action="store_true")  # List process -s
+    parser.add_argument('-o', '--poweron', help='PowerON vm with PID', action="store")  # poweron -p
+    parser.add_argument('-q', '--quitvmrx', dest='offvm', help='PowerOFF vm with Regex', action="store")  # poweron -p
+    parser.add_argument('-r', '--runvm', help='PowerON vm with Regex', action="store")  # poweron -p
+    return parser.parse_args()
 
 
-def get_full_listvm():
-    #  t = Terminal()
-    #  print(Fore.GREEN + 'Green text')
-    #  print t.red('This is red.')
+def runCmdViaSSH(vmCommand):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=host, username=user, password=secret, port=port)
-    stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/getallvms ')
-    data = stdout.read()
-    result_data = ''
-    for sT in data.splitlines():
-        m = re.match(r"^(?P<snum>\d+)\s+(?P<sname>[^\[]+)\[(?P<sdisk>[^\]]+)\]\s+(?P<spath>(?:(?!vmx).)+vmx)([^\n]+)", sT)
+    stdin, stdout, stderr = client.exec_command(vmCommand)
+    data = stdout.read() + stderr.read()
+    client.close()
+    return data
+
+
+def listVmOnVmware():
+    vmCommand = 'vim-cmd vmsvc/getallvms '
+    data = runCmdViaSSH(vmCommand)
+    resultData = ''
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=secret, port=port)
+    for i in data.splitlines():
+        m = re.match(r"^(?P<snum>\d+)\s+(?P<sname>[^\[]+)\[(?P<sdisk>[^\]]+)\]\s+(?P<spath>(?:(?!vmx).)+vmx)([^\n]+)", i)
         if m:
-            sCom = 'vim-cmd vmsvc/power.getstate ' + m.group('snum') + "\n"
-            stdin, stdout, stderr = client.exec_command(sCom)
-            result_data = stdout.read() + stderr.read()
-            if re.search(r"Powered on", result_data):
+            vmCommand = 'vim-cmd vmsvc/power.getstate ' + m.group('snum') + "\n"
+            stdin, stdout, stderr = client.exec_command(vmCommand)
+            resultData = stdout.read() + stderr.read()
+            if re.search(r"Powered on", resultData):
                 sState = "ON"
                 print (Fore.GREEN + Style.BRIGHT + "%5s    %20s     %20s    %5s" % (m.group('snum'), m.group('sname'), m.group('sdisk'), sState))
             else:
@@ -138,15 +61,30 @@ def get_full_listvm():
     client.close()
 
 
-def get_listvm(argSt):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, password=secret, port=port)
-    stdin, stdout, stderr = client.exec_command('vim-cmd vmsvc/getallvms | grep -i -E '+"\""+argSt+"\"\n")
-    data = stdout.read()
-    for sT in data.splitlines():
-        print sT
-    client.close()
+def main():
+    args = cmdArgsParser()
+    if(args.procvm):
+        vmCommand = 'esxcli vm process list'
+        data = runCmdViaSSH(vmCommand)
+        for i in data.splitlines():
+            print i
+    # Kill vm with PIDs
+    if(args.killpid):
+        for i in args.killpid.split(','):
+            vmCommand = 'vim-cmd vmsvc/power.off ' + i + "\n"
+            data = runCmdViaSSH(vmCommand)
+            print data
+    # PowerON vm with PIDs
+    if(args.poweron):
+        for i in args.poweron.split(','):
+            vmCommand = 'vim-cmd vmsvc/power.on ' + i + "\n"
+            data = runCmdViaSSH(vmCommand)
+            print data
+    # PowerON vm with PIDs
+    if(args.listvm):
+        listVmOnVmware()
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    sys.exit()
+
+if __name__ == '__main__':
+    sys.exit(main())
