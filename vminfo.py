f29a6b0e8b57d9d44ee7c0c3279b061291020efe
Alex <alex@akarpov.net>
2017-06-23 16:32:16 +0300

    Change structure programm

--- vminfo.py
+++ vminfo.py
@@ -13,21 +13,22 @@ from colorama import Fore, Style
 
 host = '192.168.180.46'
 user = 'root'
-secret = ''
+secret = ""
 port = 22
 description = "vminfo: Get and set information of VM from Vmware via ssh"
 epilog = "Alexey Karpov "
+flagVmAll = 1
 
 
 def cmdArgsParser():
+    global flagVmAll
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
-    parser.add_argument('-k', '--killpid', help='Terminate vm with PIDs', action="store")
+    parser.add_argument('-k', '--killpid', help='Terminate VM with PIDs', action="store")
+    parser.add_argument('-r', '--runvm', help='PowerON VM with PIDs', action="store")  
     parser.add_argument('-l', '--listvm', help='List vm', action="store_true")
-    parser.add_argument('-p', '--procvm', help='Process list on vmware', action="store_true")
-    parser.add_argument('-o', '--poweron', help='PowerON vm with PID', action="store") 
-    parser.add_argument('-q', '--quitvmrx', dest='offvm', help='PowerOFF vm with Regex', action="store") 
-    parser.add_argument('-r', '--runvm', help='PowerON vm with Regex', action="store")  
-    parser.add_argument('-i', '--ip', help='IP Esxi Host', action="store",  dest="host", default='192.168.180.46')
+    parser.add_argument('-p', '--procvm', help='VM running  on vmware', action="store_true") 
+    parser.add_argument('-i', '--ip', help='IP Esxi Host', action="store",  dest="host", default=host)
+    
     return parser.parse_args()
 
 
@@ -39,41 +40,44 @@ def runCmdViaSSH(vmCommand):
     data = stdout.read() + stderr.read()
     client.close()
     return data
-
+    
 
 def listVmOnVmware():
+    
+    vmCommand = 'esxcli vm process list'
+    resultData = runCmdViaSSH(vmCommand)
+    lstVmRunning = list()
+    for sLine in resultData.split("\n"):
+        if(re.match(r"^([\S]+.*)", sLine)):
+            lstVmRunning.append(sLine.strip())
+    
     vmCommand = 'vim-cmd vmsvc/getallvms '
-    data = runCmdViaSSH(vmCommand)
-    resultData = ''
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, password=secret, port=port)
-    for i in data.splitlines():
+    resultData = runCmdViaSSH(vmCommand)
+
+    for i in resultData.splitlines():
         m = re.match(r"^(?P<snum>\d+)\s+(?P<sname>[^\[]+)\[(?P<sdisk>[^\]]+)\]\s+(?P<spath>(?:(?!vmx).)+vmx)([^\n]+)", i)
         if m:
-            vmCommand = 'vim-cmd vmsvc/power.getstate ' + m.group('snum') + "\n"
-            stdin, stdout, stderr = client.exec_command(vmCommand)
-            resultData = stdout.read() + stderr.read()
-            if re.search(r"Powered on", resultData):
-                sState = "ON"
+            flagRun = 0
+            sState = "OFF"
+            for vmName in lstVmRunning:
+                if m.group('sname').strip() == vmName:
+                    flagRun = 1
+                    sState = "ON"
+              
+            if flagRun:
                 print (Fore.GREEN + Style.BRIGHT + "%5s    %20s     %20s    %5s" % (m.group('snum'), m.group('sname'), m.group('sdisk'), sState))
-            else:
-                sState = "OFF"
+            elif flagVmAll:
                 print (Fore.WHITE + Style.BRIGHT + "%5s    %20s     %20s    %5s" % (m.group('snum'), m.group('sname'), m.group('sdisk'), sState))
-    client.close()
 
 
 def main():
+    global flagVmAll
     args = cmdArgsParser()
-
-    print "Host IP address: " + host
-    print(args.host)
     
     if(args.procvm):
-        vmCommand = 'esxcli vm process list'
-        data = runCmdViaSSH(vmCommand)
-        for i in data.splitlines():
-            print i
+        flagVmAll = 0
+        listVmOnVmware()
+        
     # Kill vm with PIDs
     if(args.killpid):
         for i in args.killpid.split(','):
@@ -81,8 +85,8 @@ def main():
             data = runCmdViaSSH(vmCommand)
             print data
     # PowerON vm with PIDs
-    if(args.poweron):
-        for i in args.poweron.split(','):
+    if(args.runvm):
+        for i in args.runvm.split(','):
             vmCommand = 'vim-cmd vmsvc/power.on ' + i + "\n"
             data = runCmdViaSSH(vmCommand)
             print data
@@ -94,5 +98,5 @@ def main():
 
 if __name__ == '__main__':
     
-    secret = getpass.getpass('Password:')
+    if secret == "": secret = getpass.getpass('Password:')
     sys.exit(main())
